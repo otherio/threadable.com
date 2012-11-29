5b27c693566c0c14239f887f0fb9d3d62fafd2b9
Jared Grippe <jared@deadlyicon.com>
2012-11-29 13:03:44 -0800

    WIP

--- api/app/controllers/sessions_controller.rb
+++ api/app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < Devise::SessionsController
 
   def create
+    if request.env['HTTP_AUTHORIZATION'].blank?
+      email, password = params.values_at(:email, :password)
+      request.env['HTTP_AUTHORIZATION'] = "Basic #{::Base64.strict_encode64("#{email}:#{password}")}"
+    end
+
     resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-    return sign_in_and_redirect(resource_name, resource)
+    sign_in_and_redirect(resource_name, resource)
   end
+
+  private
 
   def sign_in_and_redirect(resource_or_scope, resource=nil)
     scope = Devise::Mapping.find_scope!(resource_or_scope)
@@ -11,10 +18,18 @@ class SessionsController < Devise::SessionsController
     sign_in(scope, resource) unless warden.user(scope) == resource
     # if we want redirects later...
     #return render :json => {:success => true, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource), :user => current_user }
-    return render :json => {:success => true, :user => current_user, :authentication_token => current_user.authentication_token }
+    render :json => {
+      :success => true,
+      :user => current_user,
+      :authentication_token => current_user.authentication_token
+    }
   end
 
   def failure
-    return render :json => {:success => false, :errors => ["Login failed."]}, :status => 401
+    render :json => {
+      :success => false,
+      :errors => ["Login failed."]
+    }, :status => 401
   end
+
 end
