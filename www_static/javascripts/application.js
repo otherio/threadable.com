5def262536d7dcbc9f56faa96680a1b5b89941e5
Jared Grippe <jared@deadlyicon.com>
2012-11-29 14:08:47 -0800

    WIP

--- www_static/javascripts/application.js
+++ www_static/javascripts/application.js
@@ -1,17 +1,13 @@
+//= require "uri"
 //= require "jquery"
 //= require "jquery-cookie"
 //= require "mustache"
 //= require "multify"
 //= require "multify/session"
+//= require "multify/authentication"
 //= require "multify/view"
 //= require "multify/view/template"
 
 
 
-
-$(function(){
-
-  Multify.authenticate('jared@change.org','password');
-
-});
-
+Multify.init();
