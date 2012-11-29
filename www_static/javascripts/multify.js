5abd4f7ce4193e6498339e6e0d808ec196f1bbd8
Jared Grippe <jared@deadlyicon.com>
2012-11-29 14:27:57 -0800

    WIP

--- www_static/javascripts/multify.js
+++ www_static/javascripts/multify.js
@@ -6,10 +6,6 @@ Multify.host = 'http://0.0.0.0:3000';
 
 Multify.init = function(){
 
-  if (Multify.session.data().authentication_token){
-    Multify.logged_in = true;
-    // Multify.user = Multify.User.get(Multify.session.data().user_id);
-  }
 
 };
 
