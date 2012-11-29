5787a673c95a8334785389d2e8ab26c564e9773f
Jared Grippe <jared@deadlyicon.com>
2012-11-29 14:54:55 -0800

    WIP

--- www_static/javascripts/multify.js
+++ www_static/javascripts/multify.js
@@ -2,10 +2,26 @@ Multify = {};
 
 Multify.host = 'http://0.0.0.0:3000';
 
-
+Multify.is_ready = false;
+Multify.ready = function(callback){
+  if (callback){
+    if (this.is_ready){ setTimeout(callback); return this; }
+    $(this).bind('ready', callback);
+  }else{
+    if (this.is_ready) return this;
+    this.is_ready = true;
+    $(this).trigger('ready');
+  }
+  return this;
+};
 
 Multify.init = function(){
 
+  if (Multify.logged_in && !Multify.current_user){
+    Multify.loadCurrentUser().success(function(){ Multify.ready(); });
+  }else{
+    Multify.ready();
+  }
 
 };
 
@@ -13,10 +29,10 @@ Multify.init = function(){
 
 
 
-$(function(){
+Multify.ready(function(){
+  console.log('Multify ready');
   if (Multify.logged_in){
-    Multify.user=  { name: 'fix me'}
-    $('body').html('<h1>Welcome back '+Multify.user.name+'</h1>');
+    $('body').html('<h1>Welcome back '+Multify.current_user.name+'</h1>');
   }else{
     $('body').html(Multify.View.render('login_form', {email:''}));
   }
