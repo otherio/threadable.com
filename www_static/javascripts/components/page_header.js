8f48813bf0959d9f828ca4b648099a7f5d711291
Jared Grippe <jared@deadlyicon.com>
2012-11-29 17:47:00 -0800

    WIP

--- www_static/javascripts/components/page_header.js
+++ www_static/javascripts/components/page_header.js
@@ -1,33 +1,32 @@
 new Component('page_header', function(self){
 
-  self.init = function(){
-    console.log('init', self, arguments);
-    self.s()
-      ('a.logout')
-        .click(function(a, event){
-          event.preventDefault();
-          Multify.logout();
-        })
-      .end
-    .end;
+  console.log('init', self, arguments);
 
-    // Multify.on('login logout', function(){
-    //   self.s().get().each(function(){
-    //     $(this).data
-    //   });
-    // });
-  };
+  self.s()
+    ('a.logout')
+      .click(function(a, event){
+        event.preventDefault();
+        Multify.logout();
+      })
+    .end
+  .end;
 
-  self.before_render = function(options){
+  // Multify.on('login logout', function(){
+  //   self.s().get().each(function(){
+  //     $(this).data
+  //   });
+  // });
+
+  self.on('before_render', function(options){
     console.log('before_render', self, arguments);
-  };
+  });
 
-  self.after_render = function(element){
+  self.on('after_render', function(element){
     console.log('after_render', self, arguments);
 
     console.log(element);
     element.css({'background-color': colors.shift() });
-  };
+  });
 
   var colors = 'red green blue purple'.split(' ');
 
