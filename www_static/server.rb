6e0d4a27204392780028bdac6739fedf6c2887f0
Jared Grippe <jared@deadlyicon.com>
2012-11-30 10:11:58 -0800

    WIP

--- www_static/server.rb
+++ www_static/server.rb
@@ -7,63 +7,59 @@ class Server < Sinatra::Base
 
   ROOT = Pathname.new File.expand_path('../',__FILE__)
 
-  set :views,         ROOT + 'views'
+  set :root,          ROOT
+  set :static,        false
   set :public_folder, ROOT + 'public'
+  set :views,         ROOT + 'views'
   set :stylesheets,   ROOT + 'stylesheets'
   set :javascripts,   ROOT + 'javascripts'
   # set :templates,     ROOT + 'javascripts'
 
   get '*' do
-    haml :application, layout: false
+    debugger;1
   end
 
-  helpers do
+  before do
+    compile_html!
+    compile_stylesheet!
+    compile_javascript!
+  end
 
-    def stylesheet
+  # get '/' do
+  #   haml :application, layout: false
+  # end
+
+  # get '/application.js' do
+  #   content_type 'application/javascript'
+  #   javascript
+  # end
+
+  # get 'application.css' do
+  #   content_type 'text/css'
+  #   stylesheet
+  # end
+
+
+  helpers do
+    def compile_html!
+      puts "compiling html"
+      html = haml(:application, layout: false)
+      ROOT.join('public/index.html').open('w'){|f| f.write(html) }
+    end
+
+    def compile_stylesheet!
+      puts "compiling stylesheet"
       load_paths = Compass.configuration.sass_load_paths + [settings.stylesheets]
       sass = File.read(settings.stylesheets + "application.sass")
       css = Sass::Engine.new(sass, :load_paths => load_paths).to_css
-      "\n#{css}\n"
+      ROOT.join('public/application.css').open('w'){|f| f.write(css) }
     end
 
-    def javascript
+    def compile_javascript!
+      puts "compiling javascript"
       javascript = sprockets_environment.find_asset("application.js").to_s
-
-      # templates = []
-      # settings.views.find do |path|
-      #   next unless path.file?
-      #   extname = path.extname
-      #   next if extname.empty?
-      #   name = path.relative_path_from(settings.views).to_s[/(.*)#{Regexp.escape(extname)}$/,1]
-      #   next if name == 'application'
-      #   value = path.read
-      #   value = case extname
-      #   when '.haml'
-      #     Haml::Engine.new(value).to_html
-      #   else
-      #     value
-      #   end
-
-      #   templates << "new View.Template(#{name.to_json}, #{value.to_json});"
-      # end
-
-      # javascript << templates.join("\n")
-
-      "\n#{javascript}\n"
+      ROOT.join('public/application.js').open('w'){|f| f.write(javascript) }
     end
-
-
-
-    # def sass name
-    #   content_type 'text/css'
-    #   sass = File.read(File.join(settings.css_folder, "#{name}.sass"))
-    #   Sass::Engine.new(sass, :load_paths => settings.css_load_paths).to_css
-    # end
-
-    # def js name
-    #   content_type 'application/javascript'
-    #   sprockets_environment.find_asset("#{name}.js").to_s
-    # end
 
     def sprockets_environment
       @sprockets_environment ||= begin
