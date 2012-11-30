beec4d10509637293d0c8fbf49c6f8efa53982a7
Jared Grippe <jared@deadlyicon.com>
2012-11-30 10:49:01 -0800

    WIP

--- www_static/lib/compiler.rb
+++ www_static/lib/compiler.rb
@@ -2,57 +2,51 @@ require 'pathname'
 require 'fileutils'
 
 class Compiler
-  ROOT = Pathname.new File.expand_path('../../',__FILE__)
-  PUBLIC = ROOT + 'public'
+  ROOT        = Pathname.new File.expand_path('../../',__FILE__)
+  PUBLIC      = ROOT + 'public'
+  HTML        = ROOT + 'views/application.haml'
+  STYLESHEETS = ROOT + 'stylesheets'
+  JAVASCRIPTS = ROOT + 'javascripts'
 
-  def initialize
-    @javascripts = ROOT + 'javascripts'
-    @stylesheets = ROOT + 'stylesheets'
-    destroy_public!
+  def self.compile_all!
     compile_html!
     compile_stylesheet!
     compile_javascript!
     copy_images!
-
   end
 
-  def destroy_public!
-    puts "destroying #{PUBLIC}"
-    FileUtils.rm_rf(PUBLIC)
-    FileUtils.mkdir(PUBLIC)
-  end
-
-  def compile_html!
+  def self.compile_html!
     puts "compiling html"
-    haml = ROOT.join('views/application.haml').read
+    haml = HTML.read
     html = Haml::Engine.new(haml).to_html
     PUBLIC.join('index.html').open('w'){|f| f.write(html) }
   end
 
-  def compile_stylesheet!
+  def self.compile_stylesheet!
     puts "compiling stylesheet"
-    load_paths = Compass.configuration.sass_load_paths + [@stylesheets]
-    sass = File.read(@stylesheets + "application.sass")
+    load_paths = Compass.configuration.sass_load_paths + [STYLESHEETS]
+    sass = File.read(STYLESHEETS + "application.sass")
     css = Sass::Engine.new(sass, :load_paths => load_paths).to_css
     PUBLIC.join('application.css').open('w'){|f| f.write(css) }
   end
 
-  def compile_javascript!
+  def self.compile_javascript!
     puts "compiling javascript"
     javascript = sprockets_environment.find_asset("application.js").to_s
     PUBLIC.join('application.js').open('w'){|f| f.write(javascript) }
   end
 
-  def sprockets_environment
+  def self.sprockets_environment
     @sprockets_environment ||= begin
       environment = Sprockets::Environment.new(Dir.pwd)
-      environment.append_path(@javascripts)
+      environment.append_path(JAVASCRIPTS)
       environment
     end
   end
 
-  def copy_images!
+  def self.copy_images!
     puts "copying images"
+    FileUtils.rm_rf(PUBLIC+'images')
     FileUtils.copy_entry(ROOT+'images', PUBLIC+'images')
   end
 
--- www_static/server.rb
+++ www_static/server.rb
@@ -8,68 +8,7 @@ class Server < Sinatra::Base
   ROOT = Pathname.new File.expand_path('../',__FILE__)
 
   set :root,          ROOT
-  set :static,        false
-  set :public_folder, ROOT + 'public'
-  set :views,         ROOT + 'views'
-  set :stylesheets,   ROOT + 'stylesheets'
-  set :javascripts,   ROOT + 'javascripts'
-  # set :templates,     ROOT + 'javascripts'
-
-  get '*' do
-    debugger;1
-  end
-
-  before do
-    compile_html!
-    compile_stylesheet!
-    compile_javascript!
-  end
-
-  # get '/' do
-  #   haml :application, layout: false
-  # end
-
-  # get '/application.js' do
-  #   content_type 'application/javascript'
-  #   javascript
-  # end
-
-  # get 'application.css' do
-  #   content_type 'text/css'
-  #   stylesheet
-  # end
-
-
-  helpers do
-    def compile_html!
-      puts "compiling html"
-      html = haml(:application, layout: false)
-      ROOT.join('public/index.html').open('w'){|f| f.write(html) }
-    end
-
-    def compile_stylesheet!
-      puts "compiling stylesheet"
-      load_paths = Compass.configuration.sass_load_paths + [settings.stylesheets]
-      sass = File.read(settings.stylesheets + "application.sass")
-      css = Sass::Engine.new(sass, :load_paths => load_paths).to_css
-      ROOT.join('public/application.css').open('w'){|f| f.write(css) }
-    end
-
-    def compile_javascript!
-      puts "compiling javascript"
-      javascript = sprockets_environment.find_asset("application.js").to_s
-      ROOT.join('public/application.js').open('w'){|f| f.write(javascript) }
-    end
-
-    def sprockets_environment
-      @sprockets_environment ||= begin
-        environment = Sprockets::Environment.new(Dir.pwd)
-        environment.append_path settings.javascripts
-        environment
-      end
-    end
-
-  end
+  set :static,        true
 
 end
 
