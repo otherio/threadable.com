471058498779981aa0f17f4394aef6bc3f3e88dc
Jared Grippe <jared@deadlyicon.com>
2012-12-01 13:57:23 -0800

    WIP

--- www_static/javascripts/multify.js
+++ www_static/javascripts/multify.js
@@ -1,23 +1,7 @@
-Multify = {};
-
-// $.extend(Multify, Backbone.Events);
-
-Multify.host = 'http://0.0.0.0:3000';
-
-
-// Multify.App = Ember.Application.create({
-//   Router: Ember.Router.extend({
-//     root: Ember.Route.extend({
-//       index: Ember.Route.extend({
-//         route: '/'
-//       })
-//     })
-//   })
-// });
-
 var App = Ember.Application.create();
 
 App.ApplicationController = Ember.Controller.extend();
+
 App.ApplicationView = Ember.View.extend({
   templateName: 'application'
 });
@@ -34,41 +18,77 @@ App.initialize();
 
 
 
+// Multify = {};
 
-// Multify.App.initialize();
-// Multify.App.get('router') // an instance of App.Router
+// // $.extend(Multify, Backbone.Events);
 
-// Multify.ready = function(callback){
-//   if (callback) return this.on('ready', callback);
-//   this.ready = function(callback){
-//     if (callback) setTimeout(callback);
-//     return this;
-//   }
-//   return this.trigger('ready');
-// };
-
-// Multify.init = function(){
-
-//   if (Multify.logged_in){
-//     Multify.trigger('login');
-//   }else{
-//     Multify.trigger('logout');
-//   }
-
-//   if (Multify.logged_in && !Multify.current_user){
-//     Multify.loadCurrentUser().success(function(){ Multify.ready(); });
-//   }else{
-//     Multify.ready();
-//   }
-
-// };
+// Multify.host = 'http://0.0.0.0:3000';
 
 
-// Multify.ready(function(){
-//   console.log('Multify ready');
-//   View.render_page();
+// // Multify.App = Ember.Application.create({
+// //   Router: Ember.Router.extend({
+// //     root: Ember.Route.extend({
+// //       index: Ember.Route.extend({
+// //         route: '/'
+// //       })
+// //     })
+// //   })
+// // });
+
+// var App = Ember.Application.create();
+
+// App.ApplicationController = Ember.Controller.extend();
+// App.ApplicationView = Ember.View.extend({
+//   templateName: 'application'
 // });
 
-// Multify.Models = {};
-// Multify.Collections = {};
-// Multify.Views = {};
+// App.Router = Ember.Router.extend({
+//   root: Ember.Route.extend({
+//     index: Ember.Route.extend({
+//       route: '/'
+//     })
+//   })
+// })
+
+// App.initialize();
+
+
+
+
+// // Multify.App.initialize();
+// // Multify.App.get('router') // an instance of App.Router
+
+// // Multify.ready = function(callback){
+// //   if (callback) return this.on('ready', callback);
+// //   this.ready = function(callback){
+// //     if (callback) setTimeout(callback);
+// //     return this;
+// //   }
+// //   return this.trigger('ready');
+// // };
+
+// // Multify.init = function(){
+
+// //   if (Multify.logged_in){
+// //     Multify.trigger('login');
+// //   }else{
+// //     Multify.trigger('logout');
+// //   }
+
+// //   if (Multify.logged_in && !Multify.current_user){
+// //     Multify.loadCurrentUser().success(function(){ Multify.ready(); });
+// //   }else{
+// //     Multify.ready();
+// //   }
+
+// // };
+
+
+// // Multify.ready(function(){
+// //   console.log('Multify ready');
+// //   View.render_page();
+// // });
+
+// // Multify.Models = {};
+// // Multify.Collections = {};
+// // Multify.Views = {};
