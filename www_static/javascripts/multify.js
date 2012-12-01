3f928bb66efabb79853e19c9ea75b1a7b4557baf
Jared Grippe <jared@deadlyicon.com>
2012-12-01 14:48:25 -0800

    WIP

--- www_static/javascripts/multify.js
+++ www_static/javascripts/multify.js
@@ -1,5 +1,6 @@
 Multify = {};
 
+_.extend(Multify, Backbone.Events);
 
 Multify.views = {};
 Multify.View = function(name, value){
@@ -7,79 +8,31 @@ Multify.View = function(name, value){
 };
 
 
-$(function(){
-  $('body').html(Multify.views.application());
+Multify.Router = Backbone.Router.extend({
+
+  routes: {
+    "help":                 "help",    // #help
+    "projects/:query":        "search",  // #search/kiwis
+    "projects/:query/p:page": "search"   // #search/kiwis/p7
+  },
+
+  help: function() {
+    ...
+  },
+
+  search: function(query, page) {
+    ...
+  }
+
 });
 
-// // $.extend(Multify, Backbone.Events);
 
-// Multify.host = 'http://0.0.0.0:3000';
+TEMP_FAKE_PROJECTS = [
+  {name:'love steve'},
+  {name:'eat sally'},
+  {name:'pickup mustard'}
+];
 
-
-// // Multify.App = Ember.Application.create({
-// //   Router: Ember.Router.extend({
-// //     root: Ember.Route.extend({
-// //       index: Ember.Route.extend({
-// //         route: '/'
-// //       })
-// //     })
-// //   })
-// // });
-
-// var App = Ember.Application.create();
-
-// App.ApplicationController = Ember.Controller.extend();
-// App.ApplicationView = Ember.View.extend({
-//   templateName: 'application'
-// });
-
-// App.Router = Ember.Router.extend({
-//   root: Ember.Route.extend({
-//     index: Ember.Route.extend({
-//       route: '/'
-//     })
-//   })
-// })
-
-// App.initialize();
-
-
-
-
-// // Multify.App.initialize();
-// // Multify.App.get('router') // an instance of App.Router
-
-// // Multify.ready = function(callback){
-// //   if (callback) return this.on('ready', callback);
-// //   this.ready = function(callback){
-// //     if (callback) setTimeout(callback);
-// //     return this;
-// //   }
-// //   return this.trigger('ready');
-// // };
-
-// // Multify.init = function(){
-
-// //   if (Multify.logged_in){
-// //     Multify.trigger('login');
-// //   }else{
-// //     Multify.trigger('logout');
-// //   }
-
-// //   if (Multify.logged_in && !Multify.current_user){
-// //     Multify.loadCurrentUser().success(function(){ Multify.ready(); });
-// //   }else{
-// //     Multify.ready();
-// //   }
-
-// // };
-
-
-// // Multify.ready(function(){
-// //   console.log('Multify ready');
-// //   View.render_page();
-// // });
-
-// // Multify.Models = {};
-// // Multify.Collections = {};
-// // Multify.Views = {};
+$(function(){
+  $('body').html(Multify.views.application({projects:TEMP_FAKE_PROJECTS}));
+});
