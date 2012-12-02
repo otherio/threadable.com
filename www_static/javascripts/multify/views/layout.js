b6df5f0195483ecf6a3ff6d2aa3802c975704422
Jared Grippe <jared@deadlyicon.com>
2012-12-01 16:45:55 -0800

    WIP

--- www_static/javascripts/multify/views/layout.js
+++ www_static/javascripts/multify/views/layout.js
@@ -1,7 +1,12 @@
 Multify.Views.Layout = Backbone.View.extend({
   initialize: function(){
+    var view = this;
+    Multify.on('logout', function(){ view.render(); });
+    Multify.on('login', function(){ view.render(); });
     this.render();
   },
+
+  el: document.body,
 
   render: function(){
     var html = Multify.templates.layout({projects:TEMP_FAKE_PROJECTS});
@@ -12,6 +17,12 @@ Multify.Views.Layout = Backbone.View.extend({
     });
 
     this.$('.main-project-list').replaceWith(this.mainProjectList.el);
+
+    this.$('a.logout').click(function(){ Multify.logout(); });
+
+    this.$('a.login').click(function(){
+      Multify.login('jared@change.org', 'password');
+    });
 
   }
 });
