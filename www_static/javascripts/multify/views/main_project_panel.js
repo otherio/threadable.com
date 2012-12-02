19080819806ad6efb32bce8ad4bce6410f44d29a
Jared Grippe <jared@deadlyicon.com>
2012-12-01 19:47:05 -0800

    WIP

---
+++ www_static/javascripts/multify/views/main_project_panel.js
@@ -0,0 +1,31 @@
+Multify.Views.MainProjectPanel = Backbone.View.extend({
+
+  className: 'main-project-panel',
+
+  initialize: function(){
+    // this.options.projects.on('all', this.render.bind(this));
+  },
+
+  render: function(){
+    var view = this, html;
+
+    html = Multify.templates.main_project_panel();
+
+    view.$el.html(html);
+
+    // view.setActiveLink();
+
+    // view.$el.find('input').keydown(function(event){
+    //   if (event.which !== 13) return;
+    //   var
+    //     element = $(this),
+    //     project_name = element.val();
+
+    //   element.val('');
+
+    //   Multify.current_user.projects.create({
+    //     name: project_name
+    //   });
+    // });
+  }
+});
