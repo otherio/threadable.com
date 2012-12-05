9946aa14c307d72d699039fc4f5bfaf284e7ef1a
Jared Grippe <jared@deadlyicon.com>
2012-12-04 22:24:27 -0800

    WIP

--- www_static/static/views/LoggedInLayout.js
+++ www_static/static/views/LoggedInLayout.js
@@ -4,12 +4,10 @@ define(function(require) {
     User = require('models/User'),
     template = require('text!templates/nav.html');
 
-  return Marionette.ItemView.extend({
+  return Marionette.Layout.extend({
     template: _.template(template),
 
-    templateHelpers: function() {
-      return this.options;
-    }
 
   });
 });
+
