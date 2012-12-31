6bf6da756cea0ffdd126bb9b32a2a2ceb114210b
Ian Baker & Nicole Aptekar <ian@sonic.net>
2012-12-30 18:20:44 -0800

    added password to dev environment too

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -1,4 +1,8 @@
 MultifyApi::Application.configure do
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
+    [u, p] == ['multify', 'frobozz horse battery staple']
+  end
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
