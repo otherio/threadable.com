4903cf35fe5f75085f2d7b7a4b491d66f50a4fcd
Nicole Aptekar <nicoletbn@gmail.com>
2013-01-20 18:52:37 -0800

    Added new demo flows, reworked pages to be more modular.

--- mockful-world/server.rb
+++ mockful-world/server.rb
@@ -37,4 +37,41 @@ get '/stories/new' do
   haml :"stories/new"
 end
 
+get '/tasks/layup-body-carbon' do
+  haml :"tasks/carbon"
+end
 
+get '/tasks/layup-body-carbon2' do
+  haml :"tasks/carbon2"
+end
+
+get '/tasks/layup-body-carbon3' do
+  haml :"tasks/carbon3"
+end
+
+get '/tasks/layup-body-carbon4' do
+  haml :"tasks/carbon4"
+end
+
+# get '/flow/1' do
+#   haml :"flow/1"
+# end
+
+
+helpers do
+  def message_list
+    return [
+      ['layup body carbon', 3, 6, '1/5/13', 'me, Be .. <b>Bob</b>', 0, 1],
+      ['install mirrors', 1, 1, '1/5/13', '<b>Yan</b>', 0, 1],
+      ['trim body panels', 0, 2, '1/4/13', 'Tom, me', 0, 1],
+      ['make wooden form for carbon layup', 2, 3, '1/4/13', 'me, Yan, <b>Tom</b>', 1, 1],
+      ['mount body on frame', 0, 5, '1/4/13', 'Yan, me, Yan', 0, 1],
+      ['install body clips', 0, 2, '1/2/13', 'Tom, Yan', 0, 1],
+      ['How are we going to build the body?', 0, 26, '1/2/13', 'Tom, Be .. Tom', 0, 0],
+      ['get mirrors', 0, 4, '1/1/13', 'Yan, Tom, me', 0, 1],
+      ['get epoxy', 0, 2, '12/28/12', 'me, Yan, Tom', 1, 1],
+      ['get release agent', 0, 4, '12/27/12', 'me, Yan, Tom', 1, 1],
+      ['get carbon and fiberglass', 0, 2, '12/26/12', 'me, Yan, Tom', 1, 1],
+    ]
+  end
+end
