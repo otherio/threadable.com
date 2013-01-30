840e9adda15ee1473825a718822f549283a3742f
Audrey Penven and Jared Grippe <multify+audrey+jared@deadlyicon.com>
2013-01-29 19:27:32 -0800

    started making a logged in and logged out homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,7 @@
 class HomepageController < ApplicationController
 
   def show
+    render :signed_in if user_signed_in?
   end
 
 end
--- app/widgets/flash_messages_widget.rb
+++ app/widgets/flash_messages_widget.rb
@@ -1,7 +1,2 @@
 class FlashMessagesWidget < Widgets::Base
-
-  def init
-    locals[:flash_messages] = @view.flash.map
-  end
-
 end
