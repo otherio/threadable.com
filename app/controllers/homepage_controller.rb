913c614d93bf97a361c551a0d8fe8e20fd262213
Jared Grippe and Nicole Aptekar <pair+jared+nicole@other.io>
2013-01-29 21:29:58 -0800

    made the layout

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,9 @@
 class HomepageController < ApplicationController
 
   def show
+    flash[:message] = 'hello'
+    flash[:error] = 'hello'
+    flash[:notice] = 'hello'
     render :signed_in if user_signed_in?
   end
 
---
+++ app/widgets/page_footer_widget.rb
@@ -0,0 +1,2 @@
+class PageFooterWidget < Widgets::Base
+end
