13c918a6f31a3d0a9ff9cf6094d519ddf8b2c1cd
Jared Grippe <pair+jared@other.io>
2013-02-10 19:03:12 -0800

    added an example feature that uses our fixtures

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -2,3 +2,8 @@ Given /^I am not logged in$/ do
   visit '/'
   page.should have_content('Multify')
 end
+
+Given /^I am "(.*?)"$/ do |name|
+  @user = User.find_by_name(name)
+  login_as(@user)
+end
