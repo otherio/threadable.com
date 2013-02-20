88391ead063712fcd026e0e91a7f9857f2ebd681
Jared Grippe <pair+jared@other.io>
2013-02-19 21:00:24 -0800

    events now require users

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,10 @@ class Event < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  attr_accessible :project, :user
+  attr_accessible :project
+  attr_accessible :user
+
+  validates_presence_of :project
+  validates_presence_of :user
 
 end
