82324d06a9a1afd186dcb12b071e2d8fd6aca16f
Ian Baker & Nicole Aptekar <pair+ian+nicole@other.io>
2013-03-04 19:15:40 -0800

    Fixed task metadata to show remove/sign up as a doer
    also added a reasonable spec

--- spec/views/widgets/task_metadata_spec.rb
+++ spec/views/widgets/task_metadata_spec.rb
@@ -2,46 +2,34 @@ require 'spec_helper'
 
 describe "task_metadata" do
 
-  let(:task_done){ true }
-
-  let(:doers){
-    2.times.map{|i|
-      double(:"doer#{i}",
-        name: "doer#{i}",
-        avatar_url: "/doer#{i}.jpg",
-      )
-    }
-  }
-
-  let :project do
-    double(:project,
-      to_param: 'project-slug',
-    )
-  end
-
-  let :task do
-    double(:task,
-      done?: task_done,
-      subject: 'CONVERSATION SUBJECT',
-      doers: doers,
-      project: project,
-      to_param: 'task-slug',
-    )
-  end
-
-  let(:user){ double(:user) }
+  let(:current_user) { FactoryGirl.create(:user) }
+  let(:task) { FactoryGirl.create(:task) }
 
   def locals
     {
       task: task,
-      user: user,
+      current_user: current_user
     }
   end
 
-  it "should equal this brital string" do
-    pending
-    # TODO replace this with better tests
-    return_value.should == "<p class='lead task-title'>\n<i class='icon-ok finished-check'></i>\nCONVERSATION SUBJECT\n</p>\n<div class='container-fluid'>\n<div class='row-fluid'>\n<div class='task-controls span12'>\n<div class='doers-and-muted'>\n<i class='icon-bolt'></i>\ndoers:\n<img alt=\"doer0\" class=\"has-tooltip\" data-toggle=\"tooltip\" src=\"/doer0.jpg\" title=\"doer0\" />\n<img alt=\"doer1\" class=\"has-tooltip\" data-toggle=\"tooltip\" src=\"/doer1.jpg\" title=\"doer1\" />\n<a class='control-link muted pull-right' href=''>\nmuted count\n</a>\n</div>\n<a class='control-link text-info toggle-doer-self' href=''>\n<i class='icon-plus'></i>\nsign me up\n</a>\n&#183;\n<a class='control-link text-info add-others' data-html data-placement='bottom' href=''>\n<i class='icon-user'></i>\nadd others\n</a>\n&#183;\n<a class='control-link text-info schedule-event' href=''>\n<i class='icon-calendar'></i>\nschedule event\n</a>\n&#183;\n<a href=\"/project-slug/conversations/task-slug?conversation%5Bdone%5D=false\" class=\"control-link text-info toggle-done\" data-method=\"PUT\" rel=\"nofollow\"><i class='icon-ok'></i>\nmark as not done\n</a><a class='control-link text-info pull-right mute-task' href=''>\n<i class='icon-volume-off'></i>\nmute conversation\n</a>\n</div>\n</div>\n</div>\n<div class='add-others-popover'>\n<input class='add-others-typeahead' data-provide='typeahead' name='add-others-typeahead' type='text'>\n<h2>things. they are real.</h2>\n</div>\n"
+  before do
+    task.project.members << current_user
+  end
+
+  context "when the current user is not a doer" do
+    it "shows a sign me up link" do
+      return_value.should =~ /sign me up/
+    end
+  end
+
+  context "when the current user is a doer" do
+    before do
+      task.doers << current_user
+    end
+
+    it "shows a remove me link" do
+      return_value.should =~ /remove me/
+    end
   end
 
 end
