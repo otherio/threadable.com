78da41dce29eaee5d25c03c7a9378d5e18b29c32
Jared Grippe <pair+jared@other.io>
2013-03-05 12:58:20 -0800

    stopped using faker and started using sequences for our user factory to try and fix an edge case we only see on CI

--- lib/test_environment/factories/user_factory.rb
+++ lib/test_environment/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name                  { Faker::Name.name }
-    email                 { Faker::Internet.email }
+    sequence(:name)       {|n| "user #{n}" }
+    sequence(:email)      {|n| "user#{n}@example.com" }
     password              'password'
     password_confirmation 'password'
   end
