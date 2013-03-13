4a7750efe14ca66125259aad208e3342cfeaa1d6
Jared Grippe <pair+jared@other.io>
2013-03-12 18:17:58 -0700

    a few small tweaks

--- lib/unsubscribe_token.rb
+++ lib/unsubscribe_token.rb
@@ -16,12 +16,12 @@ class UnsubscribeToken
 
   def decrypt
     cleartext_token = Encryptor.decrypt(Base64.urlsafe_decode64(@token), key: key, algorithm: 'bf-ofb')
-    @user_id, @project_id, epoch, _ = cleartext_token.split(':').map {|thing| thing.to_i(36)}
+    @user_id, @project_id, epoch = cleartext_token.split(':').first(3).map{|thing| thing.to_i(36)}
     @time = Time.at(epoch)
   end
 
   def encrypt
-    nonce = Random.new.rand(100000000..999999999)
+    nonce = rand(100000000..999999999)
 
     cleartext_token = [@user_id.to_s(36), @project_id.to_s(36), Time.now.to_i.to_s(36), nonce.to_s(36)].join(':')
     @token = Base64.urlsafe_encode64(Encryptor.encrypt(cleartext_token, key: key, algorithm: 'bf-ofb'))
