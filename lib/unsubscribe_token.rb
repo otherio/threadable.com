9c89760b2217dce0475392a7a6c04b273775bf1a
Ian Baker & Nicole Aptekar <pair+ian+nicole@other.io>
2013-03-17 16:46:44 -0700

    reversed order of unsub token values
    since it's a stream cipher, random data at the beginning is more useful
    than at the end.
    [Finishes #46342415]

--- lib/unsubscribe_token.rb
+++ lib/unsubscribe_token.rb
@@ -4,13 +4,13 @@ module UnsubscribeToken
 
     def encrypt project_id, user_id, time=Time.now
       nonce = rand(100000000..999999999)
-      cleartext_token = [compact(project_id), compact(user_id), compact(time.to_i), compact(nonce)].join(':')
+      cleartext_token = [compact(time.to_i), compact(nonce), compact(project_id), compact(user_id)].join(':')
       Base64.urlsafe_encode64(Encryptor.encrypt(cleartext_token, key: key, algorithm: 'bf-ofb'))
     end
 
     def decrypt token
       cleartext_token = Encryptor.decrypt(Base64.urlsafe_decode64(token), key: key, algorithm: 'bf-ofb')
-      project_id, user_id = cleartext_token.split(':').first(2).map{|s| expand(s) }
+      project_id, user_id = cleartext_token.split(':').last(2).map{|s| expand(s) }
       [project_id, user_id]
     end
 
