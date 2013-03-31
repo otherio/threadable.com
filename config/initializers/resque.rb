39b6afed1af0e880073001d9ec4ec27db164c8fb
Ian Baker <pair+ian@other.io>
2013-03-31 15:01:07 -0700

    removed heroku push debug stuff

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,2 @@
 Resque.after_fork = Proc.new { ActiveRecord::Base.connection.reconnect! }
-unless ENV.has_key? "REDISCLOUD_URL"
-  raise "missing rediscloud url"
-end
 Resque.queues # this fixed a weird edgecase
