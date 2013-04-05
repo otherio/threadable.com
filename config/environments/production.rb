470de2ea1dfd7c7746363fa638911743e8ed1516
Jared Grippe & Nicole Aptekar <pair+jared+nicole@other.io>
2013-04-05 13:59:20 -0700

    Revert "moved production to the beta domain"
    This reverts commit f761d72203504b7303e2ea9fe185ce8ab6535829.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,9 +75,9 @@ Multify::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :user_name => 'postmaster@beta.multifyapp.com',
+    :user_name => 'postmaster@multifyapp.com',
     :password => Multify.config('mailgun')['password'],
-    :domain => 'beta.multifyapp.com',
+    :domain => 'multifyapp.com',
     :address => 'smtp.mailgun.org',
     :port => 587,
     :authentication => :plain,
