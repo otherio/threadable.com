7f12ae68777b1a1dbdcca31f04f56b8f2b8c8179
Ian Baker & Nicole Aptekar <pair+ian+nicole@other.io>
2013-04-07 21:50:19 -0700

    wip: jquery function to find duplicate email text in a set of dom elements
    [#45027619]

--- app/assets/javascripts/behaviors/jquery.deduplicate.js
+++ app/assets/javascripts/behaviors/jquery.deduplicate.js
@@ -9,7 +9,7 @@
       // Get the text from text nodes and CDATA nodes
       if (element.nodeType === 3 || element.nodeType === 4) {
         // the final solution to the whitespace problem
-        var value = element.nodeValue.replace(/(\s|\n)/g, '');
+        var value = element.nodeValue.replace(/(\s|\n|\||>)/g, '');
         if(value) {
           text += value;
           domReferences[offset] = element;
@@ -18,12 +18,51 @@
         // Traverse everything else, except comment nodes
       } else if (element.nodeType !== 8) {
         var result = $(element.childNodes).textWithRefs(text.length);
-        text += result[0];
-        domReferences = _.extend(domReferences, result[1]);
+        text += result.text;
+        domReferences = _.extend(domReferences, result.references);
       }
     });
 
-    return [text, domReferences];
+    return {text: text, references: domReferences};
   };
 
+  $.fn.findDuplicates = function(toCompare) {
+    return this.each(function() {
+      var element = this;
+      var elementWithRefs = $(this).textWithRefs()
+      var elementTextArray = elementWithRefs.text.split('').reverse();
+      var offsetFromEnd = 0;
+
+      _.each(toCompare, function(compare) {
+        var thisOffsetFromEnd;
+        var compareTextArray = $(compare).textWithRefs().text.split('').reverse();
+
+        for(thisOffsetFromEnd = 0; thisOffsetFromEnd < elementTextArray.length; thisOffsetFromEnd++) {
+          if(elementTextArray[thisOffsetFromEnd] != compareTextArray[thisOffsetFromEnd]) {
+            break;
+          }
+        }
+
+        if(thisOffsetFromEnd > offsetFromEnd) {
+          offsetFromEnd = thisOffsetFromEnd;
+        }
+      });
+
+      // we have the offset.
+      var offset = elementTextArray.length - offsetFromEnd;
+      var elementRefs = elementWithRefs.references;
+      var elementOffsets = _.keys(elementRefs).sort(function(a,b){return a-b});
+      var matchIndex = _.indexOf(elementOffsets, String(offset), true);
+      var keysToSelect = [];
+
+      if(matchIndex > 0) {
+        keysToSelect = elementOffsets.slice(matchIndex);
+      } else {
+        keysToSelect = elementOffsets;
+      }
+
+      $(this).html($(_.values(_.pick(elementWithRefs.references, keysToSelect))).parent());
+    });
+
+  };
 })(jQuery);
--- spec/javascripts/behaviors/jquery.deduplicate_spec.js
+++ spec/javascripts/behaviors/jquery.deduplicate_spec.js
@@ -1,20 +1,33 @@
 describe('jquery.deduplicate', function() {
-  describe("$.textWithRefs", function(){
-    var element;
-    beforeEach(function() {
-      loadFixture('behaviors/jquery.deduplicate');
-      $element = $('.deduplicate');
-    });
+  var element;
+  beforeEach(function() {
+    loadFixture('behaviors/jquery.deduplicate');
+    $element = $('.message1');
+  });
 
-    it("returns the text of the dom node", function() {
-      // js has no /s modifier. it's stupid.
-      expect($element.textWithRefs()[0]).toEqual('foobarbaz');
+  describe("$.textWithRefs", function(){
+    it("returns text with no whitespace or quote characters", function() {
+      expect($element.textWithRefs().text).toEqual('foobarbaz');
     });
 
     it("returns accurate references to dom elements", function() {
-      var domReferences = $element.textWithRefs()[1];
+      var domReferences = $element.textWithRefs().references;
       expect(_.keys(domReferences)).toEqual(['0','3','6']);
       expect($(domReferences['3']).text()).toMatch(/bar/);
+    });
+  });
+
+  describe("$.findDuplicates", function() {
+    var secondElement;
+    beforeEach(function() {
+      $secondElement = $('.message2');
+    });
+
+    it("returns the dom nodes that are not duplicated at the end", function() {
+      var $newContent = $secondElement.findDuplicates($element);
+      debugger
+      expect($newContent.text()).not.toMatch(/unique things/);
+      expect($newContent.text()).toMatch(/bar/);
     });
   });
 
