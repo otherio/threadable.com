dfcc55503d38070dcd43822e484aa943147a01bb
Ian Baker <pair+ian@other.io>
2013-05-02 22:20:01 -0700

    changed 'data' variable to something more descriptive

--- app/mailers/conversation_mailer.rb
+++ app/mailers/conversation_mailer.rb
@@ -1,30 +1,30 @@
 # Encoding: UTF-8
 class ConversationMailer < ActionMailer::Base
 
-  def conversation_message(data)
-    @data = data
+  def conversation_message(message)
+    @message = message
 
-    subject_tag = @data[:project_slug][0..6]
+    subject_tag = @message[:project_slug][0..6]
 
-    subject = @data[:message_subject].include?("[#{subject_tag}]") ?
-      @data[:message_subject] : "[#{subject_tag}] #{@data[:message_subject]}"
+    subject = @message[:message_subject].include?("[#{subject_tag}]") ?
+      @message[:message_subject] : "[#{subject_tag}] #{@message[:message_subject]}"
 
     # add a check mark to the subect if the conversation is a task, and if the subject doesn't already include one
-    subject = "✔ #{subject}" if @data[:is_a_task] && !subject.include?("✔")
+    subject = "✔ #{subject}" if @message[:is_a_task] && !subject.include?("✔")
 
-    from = %("#{@data[:sender_name]}" <#{@data[:sender_email]}>)
+    from = %("#{@message[:sender_name]}" <#{@message[:sender_email]}>)
 
-    unsubscribe_token = UnsubscribeToken.encrypt(@data[:project_id], @data[:recipient_id])
-    @unsubscribe_url = project_unsubscribe_url(@data[:project_slug], unsubscribe_token)
+    unsubscribe_token = UnsubscribeToken.encrypt(@message[:project_id], @message[:recipient_id])
+    @unsubscribe_url = project_unsubscribe_url(@message[:project_slug], unsubscribe_token)
 
     mail(
-      :'to'          => %(#{@data[:recipient_name].inspect} <#{@data[:recipient_email]}>),
+      :'to'          => %(#{@message[:recipient_name].inspect} <#{@message[:recipient_email]}>),
       :'subject'     => subject,
       :'from'        => from,
-      :'Reply-To'    => @data[:reply_to],
-      :'Message-ID'  => @data[:message_message_id_header],
-      :'In-Reply-To' => @data[:parent_message_id_header],
-      :'References'  => @data[:message_references_header],
+      :'Reply-To'    => @message[:reply_to],
+      :'Message-ID'  => @message[:message_message_id_header],
+      :'In-Reply-To' => @message[:parent_message_id_header],
+      :'References'  => @message[:message_references_header],
     )
   end
 
