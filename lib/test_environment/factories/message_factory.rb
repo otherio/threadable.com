20deb5681a96f35054feef6d0f4f978d28231416
Ian Baker <pair+ian@other.io>
2013-05-05 09:41:02 +0900

    updated fixtures

--- lib/test_environment/factories/message_factory.rb
+++ lib/test_environment/factories/message_factory.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :message do
     subject { Faker::Company.catch_phrase }
     body_plain { Faker::HipsterIpsum.paragraph }
+    body_html { '' }
     conversation
     user
     from { user.email }
