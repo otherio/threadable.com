0b1c19d714afe0f3c6bc11a051a53f8392e1ea70
Jared Grippe <pair+jared@other.io>
2013-06-02 13:25:09 -0700

    fixed our db:populate script

--- lib/db/populate.rb
+++ lib/db/populate.rb
@@ -4,15 +4,18 @@ class Db::Populate < MethodObject
     # Other accounts
     developers =  YAML.load(Rails.root.join('config/developers.yml').read)
 
-    developers.each do |name|
+    developers.map do |name|
       first, last = name.scan(/^(.+?) (.+)/).first
       email = "#{first.downcase}@other.io"
-      User.find_by_email(email) or User.create(
-        email_addresses: [EmailAddress.new(address: email)],
+      user = User.find_by_email(email) || User.create!(
+        email: email,
         name: name,
         password: 'password',
       )
+      user.confirm!
+      user
     end
+
   end
 
 end
