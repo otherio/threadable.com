d9be7f6fc9140a077ff0678d7293feaebd71b473
Ian Baker and Jared Grippe <pair+ian+jared@other.io>
2013-06-09 23:50:25 -0700

    updated lib/env.rb to support the lack of a .env file

--- lib/env.rb
+++ lib/env.rb
@@ -1,4 +1,7 @@
-File.read(File.expand_path('../../.env', __FILE__)).split("\n").each do |line|
-  name, value = line.split('=')
-  ENV[name] = value
+path = File.expand_path('../../.env', __FILE__)
+if File.exist?(path)
+  File.read(path).split("\n").each do |line|
+    name, value = line.split('=')
+    ENV[name] = value
+  end
 end
