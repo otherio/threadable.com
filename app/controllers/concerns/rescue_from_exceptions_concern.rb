db853b8b2972d5d891ae864124c8e36e3063b64a
Jared Grippe <pair+jared@other.io>
2013-07-24 01:15:44 -0700

    added a 404 and 500 page

---
+++ app/controllers/concerns/rescue_from_exceptions_concern.rb
@@ -0,0 +1,33 @@
+module RescueFromExceptionsConcern
+  extend ActiveSupport::Concern
+
+  NOT_FOUND_EXCEPTIONS = [
+    ::ActionController::RoutingError,
+    ::ActionController::UnknownController,
+    ::AbstractController::ActionNotFound,
+    ::ActiveRecord::RecordNotFound,
+  ]
+
+
+  included do
+    # unless Rails.application.config.consider_all_requests_local
+      rescue_from Exception, with: :rescue_from_exception
+    # end
+  end
+
+  private
+
+  def rescue_from_exception exception
+    status = case exception
+    when 5 #*NOT_FOUND_EXCEPTIONS
+      404
+    else
+      500
+    end
+    respond_to do |format|
+      format.html { render template: "errors/error_#{status}", layout: 'layouts/error', status: status }
+      format.all { render nothing: true, status: status }
+    end
+  end
+
+end
