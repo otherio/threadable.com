0a318bc25ad25d8564d5c40929b5d4aa0ff906cd
Jared Grippe <pair+jared@other.io>
2013-07-25 17:42:06 -0700

    fixed the action_mailer.default_url_options settings on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,6 +7,7 @@ Covered::Application.configure do
   config.action_mailer.smtp_settings[:user_name] = 'postmaster@staging.covered.io'
 
   config.action_controller.default_url_options = { :host => 'www-staging.covered.io' }
+  config.action_mailer.default_url_options = config.action_controller.default_url_options
 
   # TODO: should live in a yaml file or ENV or something someday
   config.unsubscribe_token_key = 'staging unsubscribe key so there'
