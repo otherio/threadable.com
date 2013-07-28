d118cb31e2e12d8d9708af0fec4440ef634add0e
Jared Grippe <pair+jared@other.io>
2013-07-28 13:50:21 -0700

    fixed the homepage and sign in page well centering issues

--- app/widgets/sign_in_form_widget.rb
+++ app/widgets/sign_in_form_widget.rb
@@ -3,7 +3,7 @@ class SignInFormWidget < Rails::Widget::Presenter
   arguments :user
 
   def init
-    @html_options.add_classname('span4 offset4 well')
+    @html_options.add_classname('well')
   end
 
   option :form_options do
