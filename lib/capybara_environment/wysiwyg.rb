2b1101758c50031a37f74ca42096fb9bf8c44093
Jared Grippe <pair+jared@other.io>
2013-09-29 16:47:38 -0700

    fill_in_wysiwyg find the first visible iframe

--- lib/capybara_environment/wysiwyg.rb
+++ lib/capybara_environment/wysiwyg.rb
@@ -2,7 +2,7 @@ module CapybaraEnvironment::Wysiwyg
 
   def fill_in_wysiwyg value
     page.execute_script <<-JS
-      $('.wysihtml5-sandbox:first').contents().find("body").html(#{value.inspect});
+      $('.wysihtml5-sandbox:visible:first').contents().find("body").html(#{value.inspect});
     JS
   end
 
