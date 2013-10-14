e62d6ad2bc6dd87f5133c5d2521f499d3ad73b9b
Jared Grippe <pair+jared@other.io>
2013-10-13 17:49:39 -0700

    chromedriver does not work on my laptop so i setup the SELENIUM_BROWSER variable to override what browser selenium uses

--- lib/capybara_environment.rb
+++ lib/capybara_environment.rb
@@ -15,15 +15,21 @@ module CapybaraEnvironment
     false
   end
 
+  SELENIUM = ENV['SELENIUM'] == 'true'
+  # SELENIUM_BROWSER=safari
+  # SELENIUM_BROWSER=chrome
+  # SELENIUM_BROWSER=firefox
+  SELENIUM_BROWSER = (ENV['SELENIUM_BROWSER'].presence || :chrome).to_sym
+
   def before_suite!
     Capybara.register_driver :selenium do |app|
-      Capybara::Selenium::Driver.new(app, browser: :chrome)
+      Capybara::Selenium::Driver.new(app, browser: SELENIUM_BROWSER)
     end
 
     Capybara.exact_options          = true
     Capybara.ignore_hidden_elements = true
-    Capybara.default_driver         = ENV['SELENIUM'] == 'true' ? :selenium : :webkit
-    Capybara.javascript_driver      = ENV['SELENIUM'] == 'true' ? :selenium : :webkit
+    Capybara.default_driver         = SELENIUM ? :selenium : :webkit
+    Capybara.javascript_driver      = SELENIUM ? :selenium : :webkit
     Capybara.default_selector       = :css
     Capybara.default_wait_time      = 5
     Capybara.server_port = Rails.configuration.action_mailer.default_url_options[:port]
