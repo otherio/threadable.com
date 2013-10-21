9dd2220eee42699be3c69b77db5454e65b8504a1
Jared Grippe <pair+jared@other.io>
2013-10-20 18:26:01 -0700

    fixed the demoauth controller spec

--- spec/controllers/demo_auth_controller_spec.rb
+++ spec/controllers/demo_auth_controller_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe DemoAuthController do
 
-  let(:alice){ User.where(name: "Alice Neilson").first! }
+  let(:alice){ User.with_email('amywong.phd@gmail.com').first! }
 
   describe "GET index" do
     it "should redirect to the project conversations page" do
       get :index
-      response.should redirect_to project_conversations_path(alice.projects.first)
+      expect(assigns[:current_user]).to eq alice
+      expect(response).to redirect_to project_conversations_path(alice.projects.first)
     end
   end
 end
