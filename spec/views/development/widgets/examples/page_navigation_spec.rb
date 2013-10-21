e4b885116d0bf9f4f1d034261bc94020b28919dd
Ian Baker <pair+ian@other.io>
2013-10-20 19:20:05 -0700

    fixed spec

--- spec/views/development/widgets/examples/page_navigation_spec.rb
+++ spec/views/development/widgets/examples/page_navigation_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe "page_navigation example" do
 
-  before{ view.stub current_user: User.last! }
+  before do
+    view.stub current_user: User.last!
+    view.stub(:signup_enabled?).and_return(true)
+  end
 
   it_should_behave_like "a widget example"
 
