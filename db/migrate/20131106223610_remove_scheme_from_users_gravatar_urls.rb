dc2ca16a42bd418e389905fa4a597a9f09399838
Jared Grippe <pair+jared@other.io>
2013-11-06 14:54:32 -0800

    fixed the remove_scheme_from_users_gravatar_urls migration

--- db/migrate/20131106223610_remove_scheme_from_users_gravatar_urls.rb
+++ db/migrate/20131106223610_remove_scheme_from_users_gravatar_urls.rb
@@ -1,17 +1,31 @@
 class RemoveSchemeFromUsersGravatarUrls < ActiveRecord::Migration
-  def change
+
+  def up
 
     Covered::User.transaction do
-      Covered::User.select(:avatar_url).find_in_batches do |users|
-
+      Covered::User.select(:id, :avatar_url).find_in_batches do |users|
         users.each do |user|
           if user.avatar_url =~ %r{^http://gravatar\.com/(.+)$}
             user.update_attribute(:avatar_url, "//gravatar.com/#{$1}")
           end
         end
-
       end
     end
 
   end
+
+  def down
+
+    Covered::User.transaction do
+      Covered::User.select(:id, :avatar_url).find_in_batches do |users|
+        users.each do |user|
+          if user.avatar_url =~ %r{^//gravatar\.com/(.+)$}
+            user.update_attribute(:avatar_url, "http://gravatar.com/#{$1}")
+          end
+        end
+      end
+    end
+
+  end
+
 end
