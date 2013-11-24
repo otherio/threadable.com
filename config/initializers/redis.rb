843b4968f738aac97125ced9defb6511e97698f2
Jared Grippe <pair+jared@other.io>
2013-11-23 20:08:17 -0800

    still trying to fix sidekiq's redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 Redis.current = Redis.new Rails.application.config.redis
 
 Sidekiq.instance_variable_set(:@redis, nil)
+Sidekiq.redis = Rails.application.config.redis
 Sidekiq.configure_server do |config|
-  config.redis = Rails.application.config.redis
   config.failures_max_count = 5000
 end
