dd9eb283009439abc6235d179ad3475911d61f82
Jared Grippe <pair+jared@other.io>
2013-11-23 20:21:56 -0800

    added a migration to ensure each user has a primary email address

---
+++ db/migrate/20131124041910_ensure_each_user_has_a_primary_email_address.rb
@@ -0,0 +1,13 @@
+class EnsureEachUserHasAPrimaryEmailAddress < ActiveRecord::Migration
+  def up
+    User.find_in_batches do |users|
+      users.each do |user|
+        next if user.email_addresses.primary.present?
+        user.email_addresses.first.update(primary: true)
+      end
+    end
+  end
+
+  def down
+  end
+end
