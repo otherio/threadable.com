0b9331756b2f4eda9042bdf622584060b988ec83
Ian Baker & Nicole Aptekar <pair+ian+nicole@other.io>
2013-11-24 20:28:59 -0800

    Added account details (in theory) to the uservoice widget

--- app/assets/javascripts/logged_in_init.js
+++ app/assets/javascripts/logged_in_init.js
@@ -8,18 +8,18 @@ whenReady(function() {
     UserVoice.push(['identify', {
       email:      user.email_address, // User’s email address
       name:       user.name, // User’s real name
-      id:         user.id // Optional: Unique id of the user (if set, this should not change)
+      id:         user.id, // Optional: Unique id of the user (if set, this should not change)
 
       //created_at: 1364406966, // Unix timestamp for the date the user signed up
       //type:       'Owner', // Optional: segment your users by type
-      // account: {
-        // id:           project.id, // Optional: associate multiple users with a single account
-        // name:         project.name, // Account name
+      account: {
+        id:           project.id, // Optional: associate multiple users with a single account
+        name:         project.name, // Account name
         // created_at:   1364406966, // Unix timestamp for the date the account was created
         // monthly_rate: 9.99, // Decimal; monthly rate of the account
         // ltv:          1495.00, // Decimal; lifetime value of the account
         // plan:         'Enhanced' // Plan name for the account
-      // }
+      }
     }]);
 
     // Add default trigger to the bottom-right corner of the window:
