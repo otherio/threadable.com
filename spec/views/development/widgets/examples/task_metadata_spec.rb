5b39e1589b1516e3a6ba028328a754274fc1b2e6
Jared Grippe <pair+jared@other.io>
2013-11-27 19:11:38 -0800

    fixed a widget example spec that was using a real model

--- spec/views/development/widgets/examples/task_metadata_spec.rb
+++ spec/views/development/widgets/examples/task_metadata_spec.rb
@@ -4,7 +4,8 @@ describe "task_metadata example" do
 
   before do
     view.stub(:signed_in?).and_return(true)
-    view.stub(:current_user).and_return(Task.with_doers.first.doers.first)
+    covered.current_user_id = Task.with_doers.first.doers.first.id
+    view.stub(:current_user).and_return( covered.current_user )
   end
 
   it_should_behave_like "a widget example"
