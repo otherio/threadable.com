348ff514dbb045919f71d5fcfe98f2091d97e74e
Jared Grippe <pair+jared@other.io>
2013-12-05 19:01:03 -0800

    added tests for  spec/models/covered/message/body_spec.rb

--- spec/models/covered/message/body_spec.rb
+++ spec/models/covered/message/body_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe Covered::Message::Body do
 
+  context 'when given html and plain content' do
+    subject{ described_class.call('<p>hi</p>', 'hi')}
+    it { should == '<p>hi</p>' }
+    it { should be_html }
+  end
+
+  context 'when given just html content' do
+    subject{ described_class.call('<p>hi</p>', nil)}
+    it { should == '<p>hi</p>' }
+    it { should be_html }
+  end
+
+  context 'when given just plain content' do
+    subject{ described_class.call(nil, 'hi')}
+    it { should == 'hi' }
+    it { should_not be_html }
+  end
+
 end
