bd226da0efd22ed7d98e39a6ba47699180ec03aa
Jared Grippe <pair+jared@other.io>
2014-01-11 13:10:47 -0800

    the FronendController#show now raises a routing error when needed

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -3,6 +3,9 @@ class FrontendController < UnauthenticatedController
   layout false
 
   def show
+    if request.xhr? || !request.get? || request.accepts.exclude?(:html)
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 
 end
