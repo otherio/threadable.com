2087e8afff9ca7d2e8ba58c33152d8171d57eb4e
Ian Baker & Nicole Aptekar <pair+ian+nicole@other.io>
2014-01-13 21:48:48 -0800

    forgot the user model

---
+++ app/assets/javascripts/models/user.js
@@ -0,0 +1,9 @@
+Covered.User = RL.Model.extend({
+  id:           RL.attr('string'),
+  userId:       RL.attr('number'),
+  param:        RL.attr('string'),
+  name:         RL.attr('string'),
+  emailAddress: RL.attr('string'),
+  slug:         RL.attr('string'),
+  avatarUrl:    RL.attr('string'),
+});
