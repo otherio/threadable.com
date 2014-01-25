2a1ffe0c46988268b74b874e11396afaa2d73ca0
Jared Grippe <pair+jared@other.io>
2014-01-25 13:02:06 -0800

    the Covered::MixpanelTracker now rescues from Errno::ECONNRESET and reports it to Honeybadger so when mixpanel is down our site is still up

--- app/models/covered/mixpanel_tracker.rb
+++ app/models/covered/mixpanel_tracker.rb
@@ -1,24 +1,34 @@
 class Covered::MixpanelTracker < Covered::Tracker
 
   def track event_name, event_attributes
-    event_attributes.merge! via: covered.worker ? 'email' : 'web'
-    mixpanel.track(covered.current_user_id, *[event_name, event_attributes])
-    nil
+    recover_from_expected_errors do
+      event_attributes.merge! via: covered.worker ? 'email' : 'web'
+      mixpanel.track(covered.current_user_id, *[event_name, event_attributes])
+    end
   end
 
   def track_user_change user
-    mixpanel.people.set(user.id, {
-      '$name'    => user.name,
-      '$email'   => user.email_address.to_s,
-      '$created' => user.created_at.try(:iso8601),
-    })
-    nil
+    recover_from_expected_errors do
+      mixpanel.people.set(user.id, {
+        '$name'    => user.name,
+        '$email'   => user.email_address.to_s,
+        '$created' => user.created_at.try(:iso8601),
+      })
+    end
   end
 
   private
 
   def mixpanel
     @mixpanel ||= Mixpanel::Tracker.new(ENV.fetch('MIXPANEL_TOKEN'))
+  end
+
+  def recover_from_expected_errors
+    yield
+  rescue Errno::ECONNRESET => error
+    Honeybadger.notify(error)
+  ensure
+    return nil
   end
 
 end
