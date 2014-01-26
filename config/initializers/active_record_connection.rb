f8f7c839816f26b60a32a22a510539fe65d49c1a
Jared Grippe <pair+jared@other.io>
2014-01-25 21:21:48 -0800

    take 2

--- config/initializers/active_record_connection.rb
+++ config/initializers/active_record_connection.rb
@@ -1,2 +1,9 @@
-ActiveRecord::Base.configurations[Rails.env]["pool"] = Integer(ENV['DB_POOL']) if ENV['DB_POOL'].present?
-ActiveRecord::Base.establish_connection
+Rails.application.config.after_initialize do
+  ActiveRecord::Base.connection_pool.disconnect!
+
+  ActiveSupport.on_load(:active_record) do
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = Integer(ENV['DB_POOL']) if ENV['DB_POOL'].present?
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
