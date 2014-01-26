fc3c71ea023e170273fe327977415668627f53ac
Ian Baker <ian@sonic.net>
2014-01-25 17:47:48 -0800

    Reply now scrolls the window to the bottom when focusing the reply textarea
    [fixes #64442350]

---
+++ app/assets/javascripts/views/reply_view.js
@@ -0,0 +1,11 @@
+Covered.ReplyView = Ember.View.extend({
+  tagName: 'div',
+  classNames: 'reply well',
+
+  didInsertElement: function() {
+    $('.reply-content').focus(function() {
+      $('.conversation-show').scrollTop($('.conversation-show')[0].scrollHeight);
+    });
+  }
+
+});
