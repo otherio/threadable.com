de0b1de7038c70477405f0cff8c20a0ea41c285f
Jared Grippe <pair+jared@other.io>
2014-01-25 17:06:42 -0800

    shuffled around some JS so we can define ENV before ember

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+//= require_self
 //= require jquery
 //= require preload_current_user
 //= require uikit
@@ -8,58 +9,11 @@
 //= require handlebars
 //= require ember
 //= require ember-restless
-//= require_self
 //= require covered
 //= require debug
 
-$.ajaxSetup({
-  headers: {
-    'Accept': 'application/json',
-    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+ENV = {
+  FEATURES: {
+    'query-params-new': true
   }
-});
-
-
-Covered = Ember.Application.create({
-  // LOG_MODULE_RESOLVER: true,
-  // LOG_ACTIVE_GENERATION: true,
-  // LOG_VIEW_LOOKUPS: true,
-  LOG_TRANSITIONS: true,
-  // LOG_TRANSITIONS_INTERNAL: true,
-});
-// Ember.run.backburner.DEBUG = true;
-
-Covered.isSignedIn = function(){
-  var currentUser = Covered.currentUser;
-  return currentUser && currentUser.get && currentUser.get('isSignedIn');
-};
-
-// Ember.onerror = function(error){
-//   var notice = $('<div>');
-//   notice.text('ERROR: '+error.message);
-//   notice.css({color: 'red'});
-//   $('body').prepend(notice);
-//   console.error(error);
-// };
-
-Ember.TextField.reopen({
-  attributeBindings: ['autofocus']
-});
-
-$(document).on('click', 'a[href=""]', function(e){ e.preventDefault(); });
-
-UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/MMrElWgvuRBk0LjwGqQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
-
-// This disables all css-transitions while resizing
-$(window).on('resize', function(){
-  $('body').addClass('disable-all-transitions');
-  var timeout = $(this).data('disableAllTransitionsTimeout');
-  clearTimeout(timeout);
-  timeout = setTimeout(function(){ $('body').removeClass('disable-all-transitions'); }, 100);
-  $(this).data('disableAllTransitionsTimeout', timeout);
-});
-
-
-String.prototype.includes = function(string) {
-  return this.indexOf(string) !== -1;
 };
--- app/assets/javascripts/covered.js
+++ app/assets/javascripts/covered.js
@@ -1,3 +1,4 @@
+//= require_self
 //= require ./store
 //= require_tree ./mixins
 //= require_tree ./models
@@ -8,4 +9,55 @@
 //= require_tree ./templates
 //= require ./router
 //= require_tree ./routes
-//= require_self
+
+$.ajaxSetup({
+  headers: {
+    'Accept': 'application/json',
+    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+  }
+});
+
+
+Covered = Ember.Application.create({
+  // LOG_MODULE_RESOLVER: true,
+  // LOG_ACTIVE_GENERATION: true,
+  // LOG_VIEW_LOOKUPS: true,
+  LOG_TRANSITIONS: true,
+  // LOG_TRANSITIONS_INTERNAL: true,
+});
+// Ember.run.backburner.DEBUG = true;
+
+Covered.isSignedIn = function(){
+  var currentUser = Covered.currentUser;
+  return currentUser && currentUser.get && currentUser.get('isSignedIn');
+};
+
+// Ember.onerror = function(error){
+//   var notice = $('<div>');
+//   notice.text('ERROR: '+error.message);
+//   notice.css({color: 'red'});
+//   $('body').prepend(notice);
+//   console.error(error);
+// };
+
+Ember.TextField.reopen({
+  attributeBindings: ['autofocus']
+});
+
+$(document).on('click', 'a[href=""]', function(e){ e.preventDefault(); });
+
+UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/MMrElWgvuRBk0LjwGqQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+
+// This disables all css-transitions while resizing
+$(window).on('resize', function(){
+  $('body').addClass('disable-all-transitions');
+  var timeout = $(this).data('disableAllTransitionsTimeout');
+  clearTimeout(timeout);
+  timeout = setTimeout(function(){ $('body').removeClass('disable-all-transitions'); }, 100);
+  $(this).data('disableAllTransitionsTimeout', timeout);
+});
+
+
+String.prototype.includes = function(string) {
+  return this.indexOf(string) !== -1;
+};
