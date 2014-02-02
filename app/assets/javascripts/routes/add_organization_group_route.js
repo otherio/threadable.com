5e8af936aabd5b8c1a4d236b1138dc6967783e15
Jared Grippe <pair+jared@other.io>
2014-02-01 21:36:37 -0800

    new groups now use an unused recommended color if one is available

--- app/assets/javascripts/routes/add_organization_group_route.js
+++ app/assets/javascripts/routes/add_organization_group_route.js
@@ -1,9 +1,21 @@
 Covered.AddOrganizationGroupRoute = Ember.Route.extend({
+
+  DEFAULT_COLORS: (
+    '#D35400 #E74C3C #1ABC9C #34495E #8E44AD #27AE60 #3498DB #E67E22 '+
+    '#2980B9 #F1C40F #9B59B6 #F39C12 #2ECC71 #2C3E50 #16A085 #C0392B'
+  ).split(' '),
+
+  availableColor: function() {
+    var takenColors = this.modelFor('organization').get('groups').mapBy('color');
+
+    return this.DEFAULT_COLORS.reduce(function(availableColor, color) {
+      if (availableColor) return availableColor;
+      if (takenColors.indexOf(color) === -1) return color;
+    }, null);
+  },
+
   model: function(group) {
-    return Covered.Group.create({
-      color: "#9b59b6",
-      autoJoin: true
-    });
+    return Covered.Group.create({color: this.availableColor(), autoJoin: true});
   },
 
   renderTemplate: function() {
