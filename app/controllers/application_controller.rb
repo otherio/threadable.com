b4ed056da682138af1ff267ebdea2f92f79dc80a
Jared Grippe <pair+jared@other.io>
2014-02-02 20:07:39 -0800

    get requests are now redirected to expected host if the request host was unexpected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,21 @@ class ApplicationController < ActionController::Base
   include RescueFromExceptionsConcern
   include DebugCookie
 
+  before_action do
+    return unless request.get?
+    request_url = URI.parse(request.url)
+    legal_host = case
+    when Rails.env.production?;  'threadable.com'
+    when Rails.env.staging?;     'staging.threadable.com'
+    when Rails.env.development?; '127.0.0.1'
+    when Rails.env.test?;        '127.0.0.1'
+    else return
+    end
+    return if request_url.host === legal_host
+    request_url.host = legal_host
+    redirect_to request_url.to_s
+  end
+
   before_action :require_user_be_signed_in!
 
 end
