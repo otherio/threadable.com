cf0abbd7e4d0bdf4507c68f4198345f57cc7d257
Jared Grippe <pair+jared@other.io>
2014-02-02 19:02:09 -0800

    the groups dropdown on the compose page now closes when you click or focus outside of it

--- app/assets/javascripts/components/group_selector_component.js
+++ app/assets/javascripts/components/group_selector_component.js
@@ -12,5 +12,27 @@ Threadable.GroupSelectorComponent = Ember.Component.extend({
       this.set('open', false);
       this.get('target').send('addGroup', group);
     }
-  }
+  },
+
+  openChanged: function() {
+    var view = this;
+    if (this.get('open')) Ember.run.later(function() {
+      view.$('.groups a:first').focus();
+    });
+  }.observes('open'),
+
+  didInsertElement: function() {
+    var view = this;
+    $(document).on('click.GroupSelectorComponent', close);
+    $(document).on('focus.GroupSelectorComponent', '*', close);
+    function close(event) {
+      if ($.inArray(view.$()[0], $(event.target).parents().andSelf()) !== -1) return;
+      view.set('open', false);
+    }
+  },
+
+  willDestroyElement: function() {
+    $(document).off('.GroupSelectorComponent');
+  },
+
 });
