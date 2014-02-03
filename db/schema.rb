0203996fb1380523419ce54a21852d556bad996a
Jared Grippe <pair+jared@other.io>
2014-02-02 18:10:40 -0800

    forgot to update the schema with that last migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140201211449) do
+ActiveRecord::Schema.define(version: 20140203011551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
