8ba2ea79909fb26e958274077d333ed038d94083
Ian Baker <ian@sonic.net>
2014-02-02 23:32:56 -0800

    Fixed app controller's redirecting while under test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,15 @@ class ApplicationController < ActionController::Base
   before_action do
     return unless request.get?
     request_url = URI.parse(request.url)
-    legal_host = case
-    when Rails.env.production?;  'threadable.com'
-    when Rails.env.staging?;     'staging.threadable.com'
-    when Rails.env.development?; '127.0.0.1'
-    when Rails.env.test?;        '127.0.0.1'
+    legal_hosts = case
+    when Rails.env.production?;  ['threadable.com']
+    when Rails.env.staging?;     ['staging.threadable.com']
+    when Rails.env.development?; ['127.0.0.1']
+    when Rails.env.test?;        ['test.host', '127.0.0.1']
     else return
     end
-    return if request_url.host === legal_host
-    request_url.host = legal_host
+    return if legal_hosts.include?(request_url.host)
+    request_url.host = legal_hosts.first
     redirect_to request_url.to_s
   end
 
