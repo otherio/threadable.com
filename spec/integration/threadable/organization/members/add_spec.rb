6101c1d2ca91e16cac757e387189f3bcafa89077
Jared Grippe <pair+jared@other.io>
2014-02-06 15:40:49 -0800

    WIP

--- spec/integration/threadable/organization/members/add_spec.rb
+++ spec/integration/threadable/organization/members/add_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe Threadable::Organization::Members::Add do
 
   let(:organization){ threadable.organizations.find_by_slug! 'raceteam' }
-  let(:member_to_add) { threadable.users.find_by_email_address('amywong.phd@gmail.com') }
+  let(:member_to_add) { threadable.users.find_by_email_address!('amywong.phd@gmail.com') }
 
   describe '#call' do
-    let(:auto_join_group) { organization.groups.find_by_email_address_tag('graphic-design') }
-    let(:non_auto_join_group) { organization.groups.find_by_email_address_tag('electronics') }
+    let(:auto_join_group) { organization.groups.find_by_email_address_tag!('graphic-design') }
+    let(:non_auto_join_group) { organization.groups.find_by_email_address_tag!('electronics') }
     it 'adds the member to auto-joinable groups, but not non-auto-joinable groups' do
       organization.members.add(email_address: 'amywong.phd@gmail.com')
 
-      expect(auto_join_group.members.all).to include member_to_add
-      expect(non_auto_join_group.members.all).to_not include member_to_add
+      expect(auto_join_group.members).to include member_to_add
+      expect(non_auto_join_group.members).to_not include member_to_add
     end
   end
 
