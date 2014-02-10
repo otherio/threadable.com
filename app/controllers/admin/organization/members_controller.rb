9558b68f353278ba87ff0677e2bb674e7b1769cb
Jared Grippe <pair+jared@other.io>
2014-02-10 15:18:56 -0800

    WIP

--- app/controllers/admin/organization/members_controller.rb
+++ app/controllers/admin/organization/members_controller.rb
@@ -22,7 +22,7 @@ class Admin::Organization::MembersController < ApplicationController
 
   # PATCH /admin/organizations/:organization_slug/members/:user_id
   def update
-    member = organization.members.find_by_user_id! params[:user_id]
+    member = organization.members.find_by_user_slug! params[:user_id]
     if member.update(member_params)
       flash[:notice] = "update of #{member.formatted_email_address} membership to #{organization.name} was successful."
     else
