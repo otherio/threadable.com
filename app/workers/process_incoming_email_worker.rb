308589daa887ff3b1c1da289c0110b5d392e988b
Jared Grippe <pair+jared@other.io>
2014-02-09 16:53:46 -0800

    Updated the ProcessIncomingEmailWorker "semaphore" to actually be autonomic

--- app/workers/process_incoming_email_worker.rb
+++ app/workers/process_incoming_email_worker.rb
@@ -3,14 +3,13 @@ class ProcessIncomingEmailWorker < Threadable::Worker
   def perform! incoming_email_id
     @incoming_email = threadable.incoming_emails.find_by_id!(incoming_email_id)
 
-    if duplicate_being_processed?
+    processing! or begin
       logger.warn("Rescheduling ProcessIncomingEmailWorker job. MessageId: #{@incoming_email.message_id}")
       reschedule_for 5.seconds.from_now
       return
     end
 
     begin
-      processing!
       @incoming_email.process!
     ensure
       done_processing!
@@ -21,12 +20,8 @@ class ProcessIncomingEmailWorker < Threadable::Worker
     @redis_key ||= "#{self.class}:#{@incoming_email.message_id}"
   end
 
-  def duplicate_being_processed?
-    Threadable.redis.exists(redis_key)
-  end
-
   def processing!
-    Threadable.redis.set(redis_key, Time.now.to_i)
+    Threadable.redis.setnx(redis_key, Time.now.to_i) and \
     Threadable.redis.expire(redis_key, 30) # seconds
   end
 
--- spec/workers/process_incoming_email_worker_spec.rb
+++ spec/workers/process_incoming_email_worker_spec.rb
@@ -16,12 +16,11 @@ describe ProcessIncomingEmailWorker do
 
   context "when another message with the same message_id is not being processed" do
     before do
-      expect(Threadable.redis).to receive(:exists).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>').and_return(false)
+      expect(Threadable.redis).to receive(:setnx).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>', Time.now.to_i).and_return(true)
     end
 
     it "should find the incoming email record and pass it to threadable.process_incoming_email" do
-      expect(Threadable.redis).to receive(:set).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>', Time.now.to_i)
-      expect(Threadable.redis).to receive(:expire).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>', 30)
+      expect(Threadable.redis).to receive(:expire).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>', 30).and_return(true)
       expect(incoming_email).to receive(:process!)
       expect(Threadable.redis).to receive(:del).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>')
       perform!(42)
@@ -30,10 +29,9 @@ describe ProcessIncomingEmailWorker do
   end
   context "when another message with the same message_id is being processed" do
     before do
-      expect(Threadable.redis).to receive(:exists).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>').and_return(true)
+      expect(Threadable.redis).to receive(:setnx).with('ProcessIncomingEmailWorker:<9fe7229c2e5c88cbde6b5a6a4c99bc3fb7c2144e@example.com>', Time.now.to_i).and_return(false)
     end
     it "should reschedule itself for 5 seconds from now" do
-      expect(Threadable.redis).to_not receive(:set)
       expect(Threadable.redis).to_not receive(:expire)
       expect(incoming_email).to_not receive(:process!)
       expect(Threadable.redis).to_not receive(:del)
