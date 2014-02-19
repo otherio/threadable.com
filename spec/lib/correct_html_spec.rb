c499fc6c6ac7af1a3842fabdd42e9fbfd1e01325
Ian Baker <ian@sonic.net>
2014-02-19 00:33:19 -0800

    Only store syntactically correct html
    [finishes #66031144]

---
+++ spec/lib/correct_html_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe CorrectHtml do
+
+  delegate :call, to: :described_class
+
+  let :body do
+<<-BODY
+<div>
+  I am a thing.
+</div></div>
+BODY
+  end
+
+  let :stripped_body do
+<<-BODY
+<div>
+I am a thing.
+</div>
+BODY
+  end
+
+  it "should remove the extra tag" do
+    expect(call(body)).to eq stripped_body
+  end
+end
