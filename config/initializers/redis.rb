0138841971c55ab0fdc74701eb4fdf1dc5d71012
Ian Baker <ian@sonic.net>
2014-04-08 15:42:53 -0700

    Reverted sidekiq upgrade (d4b98875789a28) since jobs are now unreliable

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,3 +2,6 @@ Redis.current = Redis.new Rails.application.config.redis
 
 Sidekiq.instance_variable_set(:@redis, nil)
 Sidekiq.redis = Rails.application.config.redis
+Sidekiq.configure_server do |config|
+  config.failures_max_count = 5000
+end
