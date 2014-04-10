65d3653ccca405aee4a8063759705692d629b721
Jared Grippe <pair+jared@other.io>
2014-04-10 13:05:57 -0700

    fixed config/initializers/active_record_connection.rb to work with rails 4.1

--- config/initializers/active_record_connection.rb
+++ config/initializers/active_record_connection.rb
@@ -2,7 +2,7 @@ Rails.application.config.after_initialize do
   ActiveRecord::Base.connection_pool.disconnect!
 
   ActiveSupport.on_load(:active_record) do
-    config = Rails.application.config.database_configuration[Rails.env]
+    config = ActiveRecord::Base.configurations[Rails.env] or raise "no database configuration for #{Rails.env}\n#{ActiveRecord::Base.configurations.inspect}"
     config['pool'] = Integer(ENV['DB_POOL']) if ENV['DB_POOL'].present?
     ActiveRecord::Base.establish_connection(config)
   end
