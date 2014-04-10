765255eab6c938347ea2045cd8ad574c054c02d8
Jared Grippe <pair+jared@other.io>
2014-04-10 12:48:07 -0700

    fixed the bad hostname redirect before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,29 +7,30 @@ class ApplicationController < ActionController::Base
   include RescueFromExceptionsConcern
   include DebugCookie
 
-  before_action do
-    unless request.get?
-      request_url = URI.parse(request.url)
-      legal_hosts = case
-      when Rails.env.production?;  ['threadable.com']
-      when Rails.env.staging?;     ['staging.threadable.com']
-      when Rails.env.development?; ['127.0.0.1']
-      when Rails.env.test?;        ['test.host', '127.0.0.1']
-      else []
-      end
-      if !legal_hosts.include?(request_url.host)
-        request_url.host = legal_hosts.first
-        redirect_to request_url.to_s
-      end
-    end
-  end
-
+  before_action :redirect_from_unknown_host!
   before_action :ensure_mixpanel_distinct_id_is_correct!
   before_action :require_user_be_signed_in!
 
 
   def inspect
     %(#<#{self.class} #{request.request_method} #{request.url} #{params.inspect[1..-2]}>) rescue super
+  end
+
+  private
+
+  def redirect_from_unknown_host!
+    return unless request.get?
+    request_url = URI.parse(request.url)
+    legal_hosts = case
+    when Rails.env.production?;  ['threadable.com']
+    when Rails.env.staging?;     ['staging.threadable.com']
+    when Rails.env.development?; ['127.0.0.1']
+    when Rails.env.test?;        ['test.host', '127.0.0.1']
+    else return
+    end
+    return if legal_hosts.include?(request_url.host)
+    request_url.host = legal_hosts.first
+    redirect_to request_url.to_s
   end
 
 end
