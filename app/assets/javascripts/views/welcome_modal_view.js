a787cd722d35add2ec2352c87955d51d80d9f636
Jared Grippe <pair+jared@other.io>
2014-04-11 15:45:11 -0700

    clicking on the shroud behind the welcome modal now does the same thing as clicking "I'll watch later"

--- app/assets/javascripts/views/welcome_modal_view.js
+++ app/assets/javascripts/views/welcome_modal_view.js
@@ -1,4 +1,12 @@
 Threadable.WelcomeModalView = Ember.View.extend({
   templateName: 'welcome_modal',
   classNames: 'welcome-modal',
+
+  didInsertElement: function() {
+    var root = this.$(), controller = this.get('controller');
+    root.on('click', function(event) {
+      if (event.target === root[0]) controller.send('dismissWelcomeModalView');
+    });
+  },
+
 });
