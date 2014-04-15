bad40b9fa8e067ef8a244e48f2f71a9a248c56c7
Jared Grippe and Nicole Aptekar <pair+jared+nicole@other.io>
2014-04-15 14:45:55 -0700

    wip

--- spec/models/api_access_token_spec.rb
+++ spec/models/api_access_token_spec.rb
@@ -4,7 +4,10 @@ describe ApiAccessToken, fixtures: false do
 
   it { should belong_to(:user) }
   it { should validate_presence_of :user_id }
-  it { should validate_presence_of :token }
+
+  it 'auto generates a token' do
+    expect(described_class.new.token).to be_present
+  end
 
   it 'validates the uniqueness of token' do
     token1 = described_class.create(user_id: 1, token: 'a')
