21c56866931bffe1a681de153d50f7bdaa879855
Nicole Aptekar <me@nicolation.net>
2014-04-21 16:10:35 -0700

    removed code climate from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,4 @@
 ENV["RAILS_ENV"] ||= 'test'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 require File.expand_path("../../config/environment", __FILE__)
 RAILS_ROOT = Rails.root
