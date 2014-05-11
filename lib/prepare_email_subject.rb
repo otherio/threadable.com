72a40e492acdda61055cca6fc3491baa103e63f8
Ian Baker <pair+ian@other.io>
2014-05-11 16:22:51 +0200

    Removed the option of blank subject lines. Always set them since Threadable needs a subject to make a slug

--- lib/prepare_email_subject.rb
+++ lib/prepare_email_subject.rb
@@ -8,14 +8,14 @@ class PrepareEmailSubject < MethodObject
     possible_subject_tags = possible_subject_tags.map{|tag| Regexp.escape tag}.join('|')
     subject = email.subject.gsub(%r{\s*\[(#{possible_subject_tags}|task|✔|✔\uFE0E|✔\uFE0F)\](\s*)}, '\2').sub(/^\s+/, '')
     return subject[0..254] if subject.present?
-    return nil unless email.stripped_plain
+    return '(no subject)' unless email.stripped_plain
     split_body = email.stripped_plain.split(/\s+/)
     if split_body[8]
       subject = "#{split_body[0..7].join(' ').sub(/\W+$/, '')}..."
     else
       subject = split_body[0..7].join(' ').presence
     end
-    subject.nil? ? nil : subject[0..251]
+    subject.nil? ? '(no subject)' : subject[0..251]
   end
 
 end
--- spec/lib/prepare_email_subject_spec.rb
+++ spec/lib/prepare_email_subject_spec.rb
@@ -82,7 +82,7 @@ describe PrepareEmailSubject do
     context "with an empty message" do
       let(:stripped_plain) { '' }
       context "[foobar] " do
-        it { should be_nil }
+        it { should eq '(no subject)' }
       end
 
       context "[walmart]" do
@@ -93,7 +93,7 @@ describe PrepareEmailSubject do
     context "with a nil message" do
       let(:stripped_plain) { nil }
       context "[foobar] " do
-        it { should be_nil }
+        it { should eq '(no subject)' }
       end
 
       context "[walmart]" do
