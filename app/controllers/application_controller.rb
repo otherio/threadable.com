042bd3d7bcd29bc56f53f1af95ad9f5788a1a4f7
Ian Baker <pair+ian@other.io>
2014-06-16 17:22:02 -0700

    Request to organization.threadable.com (or org.threadable.local) will now redirect to the org.
    Also, put the code to redirect to a known hostname back. Seems it was removed by accident during the rails 4.1 upgrade?
    [finishes #69826518]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   include RescueFromExceptionsConcern
   include DebugCookie
 
+  before_action :redirect_from_organization_subdomain!
   before_action :redirect_from_unknown_host!
   before_action :ensure_mixpanel_distinct_id_is_correct!
   before_action :require_user_be_signed_in!
@@ -18,13 +19,31 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def redirect_from_organization_subdomain!
+    return unless request.get?
+    request_url = URI.parse(request.url)
+    return if legal_hosts.include?(request_url.host)
+    slug_blacklist = ['www', 'blog']  #TODO: make this be a real thing: https://www.pivotaltracker.com/story/show/62884858
+    subdomain = request_url.host.split('.')[0]
+    return if slug_blacklist.include?(subdomain)
+    request_url.host = legal_hosts.first
+    request_url.path = "/#{subdomain}/"
+    redirect_to request_url.to_s
+  end
+
   def redirect_from_unknown_host!
     return unless request.get?
     request_url = URI.parse(request.url)
-    legal_hosts = case
+    return if legal_hosts.include?(request_url.host)
+    request_url.host = legal_hosts.first
+    redirect_to request_url.to_s
+  end
+
+  def legal_hosts
+    case
     when Rails.env.production?;  ['threadable.com']
     when Rails.env.staging?;     ['staging.threadable.com']
-    when Rails.env.development?; ['127.0.0.1']
+    when Rails.env.development?; ['127.0.0.1', 'threadable.local']
     when Rails.env.test?;        ['test.host', '127.0.0.1']
     else return
     end
