a603244fcf1f68ffd7814e229ae27acc0261fbf5
Ian Baker and Nicole Aptekar <pair+ian+nicole@other.io>
2014-06-24 13:16:03 -0700

    only show the primary group in the conversations list when there's more than one group
    [finishes #73637524]

--- app/assets/javascripts/controllers/conversations_item_controller.js
+++ app/assets/javascripts/controllers/conversations_item_controller.js
@@ -3,6 +3,12 @@ Threadable.ConversationsItemController = Ember.ObjectController.extend(Threadabl
   currentGroupSlug: Ember.computed.alias('controllers.group.model').readOnly(),
 
   groups: function() {
-    return this.get('model.groups').rejectBy('slug', this.get('currentGroupSlug'));
+    var displayGroups = this.get('model.groups').rejectBy('slug', this.get('currentGroupSlug'));
+    if(displayGroups.length > 1) {
+      return displayGroups;
+    }
+
+    // only show the primary group when there's more than one group
+    return displayGroups.rejectBy('primary');
   }.property('model.groups')
 });
