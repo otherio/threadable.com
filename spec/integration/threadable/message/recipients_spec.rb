f4e0a65b2ed95db9c08808a736565a12cb5fea57
Ian Baker <ian@sonic.net>
2014-08-15 14:07:06 -0700

    Conversation messages are aware of whether they're first-message mailings
    [finishes #75463978]

--- spec/integration/threadable/message/recipients_spec.rb
+++ spec/integration/threadable/message/recipients_spec.rb
@@ -19,7 +19,7 @@ describe Threadable::Message::Recipients do
     context 'when it is the first message in the conversation' do
       let(:message) { conversation.messages.first }
 
-      it "includes the member with the first message subscription" do
+      it "includes the member with the first-message subscription" do
         expected_recipients = conversation.recipients.all.map(&:slug)
         expected_recipients << first_message_member.slug
         expect( recipients.all.map(&:slug) ).to include first_message_member.slug
@@ -30,7 +30,7 @@ describe Threadable::Message::Recipients do
     context 'for subsequent messages' do
       let(:message) { conversation.messages.last }
 
-      it "excludes the member with the first message subscription" do
+      it "excludes the member with the first-message subscription" do
         expected_recipients = conversation.recipients.all.map(&:slug)
         expect( recipients.all.map(&:slug) ).to match_array expected_recipients
         expect( recipients.all.map(&:slug) ).to_not include first_message_member.slug
