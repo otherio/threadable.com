54918c2ce631b478c31b6bc95151bebad4291ef0
Ian Baker <ian@sonic.net>
2014-09-15 14:16:33 -0700

    Switched to the big crazy kludgy code in ExtractEmailAddresses to deal with Mail not being able to parse the addresses we receive.

--- lib/extract_email_addresses.rb
+++ lib/extract_email_addresses.rb
@@ -1,12 +1,26 @@
 class ExtractEmailAddresses < MethodObject
 
   def call *strings
-    Mail::AddressList.new(strings.compact.map { |string| filter_invalid_characters(string) }.join(', ')).addresses.map(&:address)
-  end
+    email_addresses = strings.compact.join(', ')
 
-  def filter_invalid_characters string
-    string = string.to_ascii if string =~ /[^\u0000-\u007F]/
-    string.gsub(/[:,]/, '')
+    begin
+      Mail::AddressList.new(email_addresses).addresses.map(&:address)
+    rescue Mail::Field::ParseError
+      # sometimes people remove the quotes and leave the colon, or the phrase part contains unsupported unicode.
+      # so, this works around shortcomings in Mail's AddressListParser
+      email_addresses = email_addresses.to_ascii
+      email_addresses.gsub!(/:/, '')
+
+      # also, Mail doesn't allow a comma in the phrase part.
+      # this is fucked up. like mediawiki style. it hurts me.
+      # TODO: fix Mail's address list parser so it can't do this to us anymore
+      email_addresses.gsub!(/(\@[\w\d\.\s]+),/, '\1__COMMA__')
+      email_addresses.gsub!(/(?<!>),/, '')
+      email_addresses.gsub!(/__COMMA__/, ',')
+
+      Mail::AddressList.new(email_addresses).addresses.map(&:address)
+    end
+
   end
 
 end
