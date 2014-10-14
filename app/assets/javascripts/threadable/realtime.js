ba1ea1a2ada1ca17e5ee94d21d632f2e9a6d3a53
Ian Baker <ian@sonic.net>
2014-10-13 22:08:27 -0700

    Send ping/pong to keep the connection open.

--- app/assets/javascripts/threadable/realtime.js
+++ app/assets/javascripts/threadable/realtime.js
@@ -8,6 +8,10 @@ Threadable.realtime.connect = function(currentUser) {
   if (Threadable.realtime.socketIo) {
     Threadable.realtime.socketIo.on('connect', function() {
       Threadable.realtime.socketIo.emit('realtime_user_id_connected', { userId: userId });
+
+      setInterval(function() {
+        Threadable.realtime.socketIo.emit('ping');
+      }, 10*1000);
     });
 
     Threadable.realtime.socketIo.on('application_update', function(message) {
@@ -20,6 +24,10 @@ Threadable.realtime.connect = function(currentUser) {
 
       var update = Threadable.ApplicationUpdate.create(message);
       currentUser.get('applicationUpdates').pushObject(update);
+    });
+
+    Threadable.realtime.socketIo.on('pong', function(message) {
+      // do nothing for now. but these do come back.
     });
   }
 };
