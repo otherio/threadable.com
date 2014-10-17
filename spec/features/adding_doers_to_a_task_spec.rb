6910f43a945f0241b4b3b101e68632fffc22830a
Nicole Aptekar <me@nicolation.net>
2014-10-17 15:22:12 -0700

    fuck tasks and doers, so fuck this test

--- spec/features/adding_doers_to_a_task_spec.rb
+++ spec/features/adding_doers_to_a_task_spec.rb
@@ -1,43 +1,44 @@
 require 'spec_helper'
 
 feature "Adding doers to a task" do
+  pending "we are removing tasks soon"
 
   scenario %(adding doers to a task) do
-    sign_in_as 'alice@ucsd.example.com'
-    resize_window_to :large
+    # sign_in_as 'alice@ucsd.example.com'
+    # resize_window_to :large
 
-    within_element 'the sidebar' do
-      click_on 'My Conversations'
-    end
-    within_element 'the conversations pane' do
-      click_on 'Conversations'
-      click_on 'make wooden form for carbon layup'
-    end
+    # within_element 'the sidebar' do
+    #   click_on 'My Conversations'
+    # end
+    # within_element 'the conversations pane' do
+    #   click_on 'Conversations'
+    #   click_on 'make wooden form for carbon layup'
+    # end
 
-    expect(page).to be_at_url conversation_url('raceteam', 'my', 'make-wooden-form-for-carbon-layup')
+    # expect(page).to be_at_url conversation_url('raceteam', 'my', 'make-wooden-form-for-carbon-layup')
 
-    expect(doers).to eq Set[]
+    # expect(doers).to eq Set[]
 
-    click_element 'the change doers button'
-    first('.doer-selector li.member a', text: 'Alice Neilson').click
-    expect(page).to have_text '&add Alice Neilson as a doer'
-    click_on 'Update'
+    # click_element 'the change doers button'
+    # first('.doer-selector li.member a', text: 'Alice Neilson').click
+    # expect(page).to have_text '&add Alice Neilson as a doer'
+    # click_on 'Update'
 
-    expect(page).to have_text 'added Alice Neilson as a doer'
-    expect(doers).to eq Set['Alice Neilson']
+    # expect(page).to have_text 'added Alice Neilson as a doer'
+    # expect(doers).to eq Set['Alice Neilson']
 
-    reload!
-    expect(page).to have_text 'added Alice Neilson as a doer'
-    expect(doers).to eq Set['Alice Neilson']
+    # reload!
+    # expect(page).to have_text 'added Alice Neilson as a doer'
+    # expect(doers).to eq Set['Alice Neilson']
 
-    click_element 'the change doers button'
-    first('.doer-selector li.member a', text: 'Alice Neilson').click
-    expect(page).to have_text '&remove Alice Neilson as a doer'
-    click_on 'Update'
+    # click_element 'the change doers button'
+    # first('.doer-selector li.member a', text: 'Alice Neilson').click
+    # expect(page).to have_text '&remove Alice Neilson as a doer'
+    # click_on 'Update'
 
-    wait_for_ember!
-    expect(page).to have_text 'removed Alice Neilson as a doer'
-    expect(doers).to eq Set[]
+    # wait_for_ember!
+    # expect(page).to have_text 'removed Alice Neilson as a doer'
+    # expect(doers).to eq Set[]
   end
 
 
