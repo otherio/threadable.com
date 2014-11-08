20be3cfc99ecee8398219d26c6434ca7a3f5618c
Nicole Aptekar <me@nicolation.net>
2014-11-07 16:08:51 -0800

    switch to new window in spec
    [#81999912]

--- spec/features/held_messages_spec.rb
+++ spec/features/held_messages_spec.rb
@@ -39,6 +39,7 @@ feature "Held messages" do
     visit conversations_url(organization, 'raceteam')
     expect(page).to have_text "Held Messages"
     click_on 'held messages'
+    page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
 
     expect(page).to have_text email_subject
     expect(page).to_not have_text body
