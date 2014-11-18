0697aea4a73f84e92126d7a4feccb809e7158f21
Nicole Aptekar <me@nicolation.net>
2014-11-18 16:52:14 -0500

    made way better email action specs, because the old ones were the worst.

--- spec/integration/email_action_spec.rb
+++ spec/integration/email_action_spec.rb
@@ -49,18 +49,136 @@ describe EmailAction, :type => :request do
 
   describe '#execute!' do
     let(:email_action) { described_class.new(threadable, type, user.id, record.id) }
+    before do
+      user.update(secure_mail_buttons: true)
+    end
 
     describe 'mute' do
       let(:type) { :mute }
       let(:record) { organization.conversations.find_by_slug('drive-trains-are-expensive') }
 
+
       it 'mutes the conversation' do
+        expect(conversation).to_not be_muted_by user
         email_action.execute!
         conversation.reload
         expect(conversation).to be_muted_by user
+        assert_tracked(user.id, 'Email action taken', type: "mute", record_id: conversation.id)
+      end
+    end
+
+    describe 'done' do
+      let(:type) { :done }
+      let(:task) { organization.tasks.find_by_slug('get-a-new-soldering-iron') }
+      let(:record) { organization.tasks.find_by_slug('get-a-new-soldering-iron') }
+
+      it 'finishes the task' do
+        expect(task).to_not be_done
+        email_action.execute!
+        task.reload
+        expect(task).to be_done
+        assert_tracked(user.id, 'Email action taken', type: "done", record_id: task.id)
+      end
+    end
+
+    describe 'undone' do
+      let(:type) { :undone }
+      let(:record) { organization.tasks.find_by_slug('layup-body-carbon') }
+
+      it 'finishes the task' do
+        expect(task).to be_done
+        email_action.execute!
+        task.reload
+        expect(task).to_not be_done
+        assert_tracked(user.id, 'Email action taken', type: "undone", record_id: task.id)
+      end
+    end
+
+    describe 'follow' do
+      let(:type) { :follow }
+      let(:record) { organization.conversations.find_by_slug('drive-trains-are-expensive') }
+
+      it 'follows the conversation' do
+        expect(conversation).to_not be_followed_by user
+        email_action.execute!
+        conversation.reload
+        expect(conversation).to be_followed_by user
+        assert_tracked(user.id, 'Email action taken', type: "follow", record_id: conversation.id)
+      end
+    end
+
+    describe 'unfollow' do
+      let(:type) { :unfollow }
+      let(:record) { organization.conversations.find_by_slug('drive-trains-are-expensive') }
+
+      before do
+        conversation.follow_for user
+      end
+
+      it 'unfollows the conversation' do
+        expect(conversation).to be_followed_by user
+        email_action.execute!
+        conversation.reload
+        expect(conversation).to_not be_followed_by user
+        assert_tracked(user.id, 'Email action taken', type: "unfollow", record_id: conversation.id)
+      end
+    end
+
+    describe 'add' do
+      let(:type) { :add }
+      let(:task) { organization.tasks.find_by_slug('install-mirrors') }
+      let(:record) { organization.tasks.find_by_slug('install-mirrors') }
+
+      it 'adds a doer' do
+        expect(task.doers).to_not include user
+        email_action.execute!
+        task.reload
+        expect(task.doers).to include user
+        assert_tracked(user.id, 'Email action taken', type: "add", record_id: task.id)
+      end
+    end
+
+    describe 'remove' do
+      let(:type) { :remove }
+      let(:task) { organization.tasks.find_by_slug('get-a-new-soldering-iron') }
+      let(:record) { organization.tasks.find_by_slug('get-a-new-soldering-iron') }
+
+      it 'removes a doer' do
+        expect(task.doers).to include user
+        email_action.execute!
+        task.reload
+        expect(task.doers).to_not include user
+        assert_tracked(user.id, 'Email action taken', type: "remove", record_id: task.id)
+      end
+    end
+
+    describe 'join' do
+      let(:type) { :join }
+      let(:group){ organization.groups.find_by_slug!('fundraising') }
+      let(:record) { organization.groups.find_by_slug!('fundraising') }
+
+
+      it 'joins the group' do
+        expect(group.members).to_not include user
+        email_action.execute!
+        group.reload
+        expect(group.members).to include user
+        assert_tracked(user.id, 'Email action taken', type: "join", record_id: group.id)
+      end
+    end
+
+    describe 'leave' do
+      let(:type) { :leave }
+      let(:record) { organization.groups.find_by_slug!('electronics') }
+
+      it 'leaves the group' do
+        expect(group.members).to include user
+        email_action.execute!
+        group.reload
+        expect(group.members).to_not include user
+        assert_tracked(user.id, 'Email action taken', type: "leave", record_id: group.id)
       end
     end
 
   end
-
 end
