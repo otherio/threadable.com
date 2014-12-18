c9de90cf078473e96d0fe4f14154c5acbe1cd452
Ian Baker <ian@sonic.net>
2014-12-17 17:07:44 -0800

    disable mixpanel tracking

--- app/models/threadable/mixpanel_tracker.rb
+++ app/models/threadable/mixpanel_tracker.rb
@@ -21,7 +21,7 @@ class Threadable::MixpanelTracker < Threadable::Tracker
   def track_for_user tracking_id, event_name, event_attributes={}
     recover_from_expected_errors do
       event_attributes.merge! via: threadable.worker ? 'email' : 'web'
-      mixpanel.track(tracking_id, event_name, event_attributes)
+      # mixpanel.track(tracking_id, event_name, event_attributes)
     end
   end
 
