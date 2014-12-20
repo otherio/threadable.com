ff6edaa609ab57a2115600f45ac683399cef45b5
Nicole Aptekar <me@nicolation.net>
2014-12-19 17:37:51 -0800

    you should not be allowed to see organizations you are not active in. duh. This could be tested elsewhere, but that's what I got

--- spec/serializers/current_user_serializer_spec.rb
+++ spec/serializers/current_user_serializer_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe CurrentUserSerializer do
 
   let(:alice) { threadable.users.find_by_email_address!('alice@ucsd.example.com') }
+  let(:bob) { threadable.users.find_by_email_address!('bob@ucsd.example.com') }
   let(:marcus){ threadable.users.find_by_email_address!('marcus@sfhealth.example.com') }
 
   let(:expected_key){ :user }
@@ -25,6 +26,29 @@ describe CurrentUserSerializer do
         external_authorizations: serialize_model(:external_authorizations, alice.external_authorizations.all).values.first,
         current_organization_id: alice.current_organization_id,
         organizations:           serialize_model(:light_organizations, alice.organizations.all).values.first,
+        dismissed_welcome_modal: true,
+      )
+    end
+  end
+
+  context 'when signed in as bob, who has been removed forcefully from the org' do
+    before do
+      threadable.current_user = alice
+      alice.organizations.all.first.members.find_by_email_address('bob@ucsd.example.com').remove
+      threadable.current_user = bob
+    end
+    it do
+      is_expected.to eq(
+        id:                      'current',
+        user_id:                 bob.user_id,
+        param:                   bob.to_param,
+        name:                    bob.name,
+        email_address:           bob.email_address.to_s,
+        slug:                    bob.slug,
+        avatar_url:              bob.avatar_url,
+        external_authorizations: [],
+        current_organization_id: bob.current_organization_id,
+        organizations:           [],
         dismissed_welcome_modal: true,
       )
     end
